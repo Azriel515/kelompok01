6e69b58ad44de1f24fff3180d7e2a1d013c777a6
Azriel515 <azriel.ilham1234@gmail.com>
2024-07-12 17:33:33 +0800

    Menerapkan perulangan untuk mengatur banyak siswa

--- codingan.cpp
+++ codingan.cpp
@@ -29,23 +29,31 @@ void Hitung_Ambang_Nilai(Siswa siswa, float NilaiAkhir) {
 }
 
 int main() {
-    Siswa siswa;
-    cout << "Masukkan Nama Siswa: ";
-    getline(cin, siswa.nama); 
-    cout << "Masukkan Nilai Tugas: ";
-    cin >> siswa.NilaiTugas;
-    cout << "Masukkan Nilai Kuis: ";
-    cin >> siswa.NilaiKuis;
-    cout << "Masukkan Nilai Ujian: ";
-    cin >> siswa.NilaiUjian;
+    char lanjut;
+    
+    do {
+        Siswa siswa;
+        cout << "Masukkan Nama Siswa: ";
+        getline(cin, siswa.nama); 
+        cout << "Masukkan Nilai Tugas: ";
+        cin >> siswa.NilaiTugas;
+        cout << "Masukkan Nilai Kuis: ";
+        cin >> siswa.NilaiKuis;
+        cout << "Masukkan Nilai Ujian: ";
+        cin >> siswa.NilaiUjian;
 
-    cin.ignore();
+        cin.ignore();
 
-    float NilaiAkhir = Hitung_Nilai_Akhir(siswa);
-    cout << "Nilai Akhir " << siswa.nama << " Adalah: " << NilaiAkhir << endl;
+        float NilaiAkhir = Hitung_Nilai_Akhir(siswa);
+        cout << "Nilai Akhir " << siswa.nama << " Adalah: " << NilaiAkhir << endl;
 
-    Hitung_RataRata_Bobot(siswa);
-    Hitung_Ambang_Nilai(siswa, NilaiAkhir);
+        Hitung_RataRata_Bobot(siswa);
+        Hitung_Ambang_Nilai(siswa, NilaiAkhir);
+
+        cout << "Apakah Anda Ingin Melanjutkan input data siswa yang lain? (y/n): ";
+        cin >> lanjut;
+        cin.ignore();
+    } while (lanjut == 'y' || lanjut == 'Y');
 
     return 0;
 }
